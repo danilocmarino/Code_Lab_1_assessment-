fcff72a6ae34e5d0853222510702ee2bfada78a5
danilocmarino <danilo.cunha.marino@hotmail.com>
2025-11-20 15:23:47 +0000

    This code output Hello.

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+void hello() { //Function which hello is defined. 
+  cout << "Hello" << endl; //Output the function
+}
+
+int main() {
+  hello(); //Call the function
+  return 0;
+}
