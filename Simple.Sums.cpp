5222b5a35d6c60d8eea0abfa545730bc02e09c7a
danilocmarino <danilo.cunha.marino@hotmail.com>
2025-11-20 15:12:10 +0000

    This code does a simple sum.

---
+++ Simple.Sums.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 8;    //A is a integer which has value 8//  
+    int b = 10;   //B is a integer which has value 10//
+    int c = a + b;  //C is a integer which collects A and B, and then, soma both integer//
+    cout << c << endl; //This comand prints the C integer result// 
+} 
